6b2942e775a45317f2880bc8ff6daf81a37a301c
ChromaMaster <vlad.sabi18@gmail.com>
2019-04-11 10:31:24 +0200

    Fix address properties omission

--- main.py
+++ main.py
@@ -3,61 +3,117 @@ import sys
 import json
 import yaml
 
+
+def fetch_vlans(dir_path, vlans):
+    for vlan in vlans:
+        filename = "{}/vlan{}".format(dir_path, vlan["name"])
+        with(open(filename, "w")) as f:
+            text = (
+                "auto {device}.{name}\n" +
+                "iface {device}.{name} int manual"
+            ).format(**vlan)
+            f.write(text)
+
+
+def fetch_bridges(dir_path, bridges):
+    for bridge in bridges:
+        filename = "{}/{}".format(dir_path, bridge["name"])
+
+        bridge_mode = "manual"
+        orphan_bridge = False
+
+        if("device" not in bridge):
+            orphan_bridge = True
+
+        # If th bridge has IP address it will be set as static config
+        if("network_config" in bridge):
+            bridge_mode = "static"
+            netconfig = bridge["network_config"]
+
+        with(open(filename, "w")) as f:
+            text = "auto {}\n".format(bridge["name"])
+            text += "iface {} inet {}\n".format(bridge["name"], bridge_mode)
+
+            # Add the bridge IP config
+            if(bridge_mode == "static"):            
+                for key, value in netconfig.items():
+                    text += f"\t{key} {value}\n"
+
+            # if(not orphan_bridge):
+            text += (
+                "\tbridge_ports {device}\n" +
+                "\tbidge_stp off\n" +
+                "\tbridge_fd 0\n" +
+                "\tbridge_maxwait 0\n"
+            ).format(**bridge)
+
+            f.write(text)
+
+
 def main(data_path, file_type):
-    
+
     with(open(data_path, "r")) as f:
         if(file_type == "json"):
             data = json.load(f)
         else:
             data = yaml.load(f, yaml.SafeLoader)
-    
+
     for machine in data:
-        dirpath = machine["name"]
+        dir_path = machine["name"]
 
         # Creates a directory to store network config files
-        if(not os.path.exists(dirpath)):
-            os.mkdir(dirpath)
+        if(not os.path.exists(dir_path)):
+            os.mkdir(dir_path)
 
         # Look for vlans
-        for vlan in machine["vlans"]:
-            filename = "{}/vlan{}".format(dirpath, vlan["name"])
-            with(open(filename, "w")) as f:
-                text =(
-                    "auto {device}.{name}\n" + 
-                    "iface {device}.{name} int manual"                    
-                ).format(**vlan)                
-                f.write(text)
+        fetch_vlans(dir_path, machine["vlans"])
+
+        # for vlan in machine["vlans"]:
+        #     filename = "{}/vlan{}".format(dirpath, vlan["name"])
+        #     with(open(filename, "w")) as f:
+        #         text =(
+        #             "auto {device}.{name}\n" +
+        #             "iface {device}.{name} int manual"
+        #         ).format(**vlan)
+        #         f.write(text)
 
         # Look for bridges
-        for bridge in machine["bridges"]:
-            filename = "{}/{}".format(dirpath, bridge["name"])
-            bridge_mode = "manual"
+        fetch_bridges(dir_path, machine["bridges"])
+        # for bridge in machine["bridges"]:
+        #     filename = "{}/{}".format(dirpath, bridge["name"])
+        #     bridge_mode = "manual"
+        #     orphan_bridge = False
 
-            # If th bridge has IP address it will be set as static config
-            if("network_config" in bridge):
-                bridge_mode = "static"
-                netconfig = bridge["network_config"]
-        
-            with(open(filename, "w")) as f:            
-                text  = "auto {}\n".format(bridge["name"])
-                text += "iface {} inet {}\n".format(bridge["name"], bridge_mode)
-                
-                # Add the bridge IP config
-                if(bridge_mode == "static"):
-                    text += (
-                        "\taddress {address}\n" + 
-                        "\tnetmask {netmask}\n" +
-                        "\tgatseway {gateway}\n"
-                    ).format(**netconfig)
-                    
-                text += (
-                    "\tbridge_ports {device}\n" +
-                    "\tbidge_stp off\n" +
-                    "\tbridge_fd 0\n" +
-                    "\tbridge_maxwait 0\n"
-                ).format(**bridge)
+        #     if("device" not in bridge):
+        #         orphan_bridge = True
 
-                f.write(text)
+        #     # If th bridge has IP address it will be set as static config
+        #     if("network_config" in bridge):
+        #         bridge_mode = "static"
+        #         netconfig = bridge["network_config"]
+
+        #     with(open(filename, "w")) as f:
+        #         text  = "auto {}\n".format(bridge["name"])
+        #         text += "iface {} inet {}\n".format(bridge["name"], bridge_mode)
+
+        #         # Add the bridge IP config
+        #         if(bridge_mode == "static"):
+        #             text += (
+        #                 "\taddress {address}\n" +
+        #                 "\tnetmask {netmask}\n" +
+        #                 "\tgateway {gateway}\n"
+        #             ).format(**netconfig)
+
+        #         if(not orphan_bridge):
+        #             text += (
+        #                 "\tbridge_ports {device}\n" +
+        #                 "\tbidge_stp off\n" +
+        #                 "\tbridge_fd 0\n" +
+        #                 "\tbridge_maxwait 0\n"
+        #             ).format(**bridge)
+
+        #         f.write(text)
+
 
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
@@ -69,10 +125,9 @@ if __name__ == "__main__":
     # Gets the extension and remove the dot (eg. .json -> json)
     extension = os.path.splitext(sys.argv[1])[1]
     extension = extension[1:]
-    
+
     if extension not in available_extensions:
         print("Error: data file format, must be <{}>".format(available_extensions))
         sys.exit()
 
     main(sys.argv[1], extension)
-
